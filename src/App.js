3b3220ad2d25bdfb72ddae41c5b7f2e89e356be0
sjhartley <89655207+sjhartley@users.noreply.github.com>
2024-09-22 15:18:00 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen flex justify-center items-center">
+          <button onClick={() => alert("hello world")} class="bg-orange-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>       
     </div>
   );
 }
