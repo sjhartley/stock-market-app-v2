e6927d6943dfc2d29c50750655e98c5e15b84cf7
sjhartley <89655207+sjhartley@users.noreply.github.com>
2025-04-28 14:37:41 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import Watchlist from "./routes/watchlist";
+import Charts from "./routes/charts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-
       <Route path="watchlist" element={<Watchlist />} />
+      <Route path="charts" element={<Charts />} />
     </Routes>
   </BrowserRouter>
 );
